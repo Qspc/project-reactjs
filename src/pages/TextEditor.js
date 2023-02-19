b13e5e75114caf91c998f7f09e20b602814369b7
shibgotalloh <shibgotalloh_qqq@apps.ipb.ac.id>
2023-02-19 11:29:29 +0700

    create: make  text-editor function

--- src/pages/TextEditor.js
+++ src/pages/TextEditor.js
@@ -1,11 +1,28 @@
 import React from 'react';
+import { useState } from 'react';
+import 'react-quill/dist/quill.snow.css';
+import ReactQuill from 'react-quill';
+// import { useNavigate } from 'react-router-dom';
 
-const TextEditor = () => {
+export default function TextEditor() {
+  const [text, setText] = useState('');
+  // const [kata, setKata] = useState('halo');
   return (
-    <div>
-      <h1>Disini fungsi pembuatan text editor</h1>
+    <div style={{ textAlign: 'center', display: 'flex', flexDirection: 'column' }}>
+      <h1>TextEditor</h1>
+      <ReactQuill theme="snow" name="description" value={text} onChange={setText} style={{ width: '750px', height: '200px', margin: 'auto' }} />
+      <h3 style={{ padding: '40px' }}>Result: {text} </h3>
+      {/* dangerouslySetInnerHTML={{ __html: text }} */}
     </div>
   );
-};
+}
 
-export default TextEditor;
+// const TextEditor = () => {
+//   return (
+//     <div>
+//       <h1>Disini fungsi pembuatan text editor</h1>
+//     </div>
+//   );
+// };
+
+// export default TextEditor;
