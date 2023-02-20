6427c0fa37d0e61f3e40042d92bf3fc112a5d8a0
shibgotalloh <shibgotalloh_qqq@apps.ipb.ac.id>
2023-02-20 10:43:56 +0700

    create: make carousel func

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Carousel from './pages/Carousel';
 import TextEditor from './pages/TextEditor';
 import Home from './pages/Home';
 import PaginationFunc from './pages/Pagination';
+import CarouselFunc from './pages/Carousel';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/carousel" element={<Carousel />} />
+        <Route path="/carousel" element={<CarouselFunc />} />
         <Route path="/text-editor" element={<TextEditor />} />
         <Route path="/pagination" element={<PaginationFunc />} />
       </Routes>
--- src/pages/Carousel.js
+++ src/pages/Carousel.js
@@ -1,11 +1,30 @@
 import React from 'react';
+import 'react-responsive-carousel/lib/styles/carousel.min.css'; // requires a loader
+import { Carousel } from 'react-responsive-carousel';
 
-const Carousel = () => {
+export default function CarouselFunc() {
   return (
-    <div>
-      <h1>Fungsi carousel</h1>
-    </div>
+    <>
+      <Carousel>
+        <div style={{ margin: 'auto' }}>
+          <img src="https://source.unsplash.com/random/500x300" />
+          <p className="legend" style={{ width: '90%' }}>
+            Legend 1
+          </p>
+        </div>
+        <div style={{ margin: 'auto' }}>
+          <img src="https://source.unsplash.com/random/900x300" />
+          <p className="legend" style={{ width: '90%' }}>
+            Legend 2
+          </p>
+        </div>
+        <div style={{ margin: 'auto' }}>
+          <img src="https://source.unsplash.com/random/500x300" />
+          <p className="legend" style={{ width: '90%' }}>
+            Legend 3
+          </p>
+        </div>
+      </Carousel>
+    </>
   );
-};
-
-export default Carousel;
+}
