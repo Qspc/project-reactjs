f46ce6ff0c510cfd6b55cf8ed150f4e0333add19
shibgotalloh <shibgotalloh_qqq@apps.ipb.ac.id>
2023-03-16 19:58:31 +0700

    update: create navigate in home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,36 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
   return (
     <div>
       <h1>All Project using ReactJs</h1>
+      <ul>
+        <li>
+          <Link to="/carousel">Carousel</Link>
+        </li>
+        <li>
+          <Link to="/filter">Filtering</Link>
+        </li>
+        <li>
+          <Link to="/form">Form</Link>
+        </li>
+        <li>
+          <Link to="/pagination">Pagination</Link>
+        </li>
+        <li>
+          <Link to="/search">Search input</Link>
+        </li>
+        <li>
+          <Link to="/table">Table sorting</Link>
+        </li>
+        <li>
+          <Link to="/text-editor">Text editor</Link>
+        </li>
+        <li>
+          <Link to="/update">Update sorting</Link>
+        </li>
+      </ul>
     </div>
   );
 };
--- src/pages/SearchInput.js
+++ src/pages/SearchInput.js
@@ -4,18 +4,28 @@ import axios from 'axios';
 export default function SearchInput() {
   const [data, setData] = useState([]);
   const [searchResult, setSearchResult] = useState('');
+  const [result, setResult] = useState('');
   const filtered = useMemo(() => {
     if (searchResult) {
-      return data.filter((item) => Object.values(item.name.common).join('').toLowerCase().includes(searchResult.toLowerCase()));
+      const newData = data.filter((item) => Object.values(item.name.common).join('').toLowerCase().includes(searchResult.toLowerCase()));
+      // setResult(newData.length);
+      return newData;
     }
+    // setResult(data.length);
     return data;
   }, [searchResult, data]);
 
   useEffect(() => {
     axios
       .get('https://restcountries.com/v3.1/all?fields=name')
-      .then((res) => setData(res.data))
+      .then((res) => {
+        setData(res.data);
+        setResult(data.length);
+      })
       .catch((error) => console.log(error));
+
+    // const newResul = data.name.common;
+    // setResult(newResul.length);
   }, []);
   //   const [data, setData] = useState([]);
   //   const [searchResult, setSearchResult] = useState('');
@@ -45,6 +55,7 @@ export default function SearchInput() {
       <input onChange={(e) => setSearchResult(e.target.value)} type="text" placeholder="Search Countries..." />
       <div>
         <h1>List of animal:</h1>
+        <h4> {result} result </h4>
         <ul>
           {filtered.map((data, index) => (
             <li key={index}> {data.name.common} </li>
