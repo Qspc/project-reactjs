b992a1d83284bde8d205bd56a4468be70ac0c3f7
shibgotalloh <shibgotalloh_qqq@apps.ipb.ac.id>
2023-03-19 13:00:54 +0700

    update: fix bug for count sorting

--- src/pages/SearchInput.js
+++ src/pages/SearchInput.js
@@ -4,15 +4,23 @@ import axios from 'axios';
 export default function SearchInput() {
   const [data, setData] = useState([]);
   const [searchResult, setSearchResult] = useState('');
-  const [result, setResult] = useState('');
-  const filtered = useMemo(() => {
+  const [result, setResult] = useState([]);
+  let filtered = useMemo(() => {
     if (searchResult) {
       const newData = data.filter((item) => Object.values(item.name.common).join('').toLowerCase().includes(searchResult.toLowerCase()));
-      // setResult(newData.length);
-      return newData;
+      return newData.length;
     }
-    // setResult(data.length);
-    return data;
+    return data.length;
+  }, [searchResult, data]);
+  useEffect(() => {
+    if (searchResult) {
+      const newData = data.filter((item) => Object.values(item.name.common).join('').toLowerCase().includes(searchResult.toLowerCase()));
+      // setCountry(newData);
+      setResult(newData);
+    } else {
+      // console.log(data);
+      setResult(data);
+    }
   }, [searchResult, data]);
 
   useEffect(() => {
@@ -20,44 +28,18 @@ export default function SearchInput() {
       .get('https://restcountries.com/v3.1/all?fields=name')
       .then((res) => {
         setData(res.data);
-        setResult(data.length);
       })
       .catch((error) => console.log(error));
-
-    // const newResul = data.name.common;
-    // setResult(newResul.length);
   }, []);
-  //   const [data, setData] = useState([]);
-  //   const [searchResult, setSearchResult] = useState('');
-  //   const [filtered, setFiltered] = useState(data);
-  //   useEffect(() => {
-  //     axios
-  //       .get('https://restcountries.com/v3.1/all/?fields=name')
-  //       .then((res) => {
-  //         setData(res.data);
-  //         console.log(searchResult);
-  //         if (searchResult !== '') {
-  //           const filteredData = data.filter((item) => {
-  //             return Object.values(item.name.common).join('').toLowerCase().includes(searchResult.toLowerCase());
-  //           });
-  //           setFiltered(filteredData);
-  //         } else {
-  //           setFiltered(data);
-  //         }
-  //       })
-  //       .catch((e) => {
-  //         console.log(e);
-  //       });
-  //   }, [searchResult, setSearchResult]);
 
   return (
     <>
       <input onChange={(e) => setSearchResult(e.target.value)} type="text" placeholder="Search Countries..." />
       <div>
         <h1>List of animal:</h1>
-        <h4> {result} result </h4>
+        <h4> {filtered} result </h4>
         <ul>
-          {filtered.map((data, index) => (
+          {result.map((data, index) => (
             <li key={index}> {data.name.common} </li>
           ))}
         </ul>
