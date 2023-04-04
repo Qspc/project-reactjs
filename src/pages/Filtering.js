47d30c242b20fc1f37cbfa2237eabfad8aec3806
shibgotalloh <shibgotalloh_qqq@apps.ipb.ac.id>
2023-04-04 13:48:54 +0700

    update: styling in filtering

--- src/pages/Filtering.js
+++ src/pages/Filtering.js
@@ -60,10 +60,15 @@ export default function Filtering() {
 
   return (
     <>
-      <div>
-        Select :
-        <select onChange={handleChange} defaultValue={nilai} name="negara" id="negara">
-          <option value=""></option>
+      <div style={{ textAlign: 'center' }}>
+        <select
+          style={{ display: 'inline-block', width: '160px', height: '34px', lineHeight: '30px', position: 'relative', margin: '15px 0px', border: '0.1px solid white', boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.07)' }}
+          onChange={handleChange}
+          defaultValue={nilai}
+          name="negara"
+          id="negara"
+        >
+          <option value="">Select Country:</option>
           <option value="Inggris">Inggris</option>
           <option value="Spanyol">Spanyol</option>
           <option value="Italia">Italia</option>
@@ -71,7 +76,20 @@ export default function Filtering() {
       </div>
       <div style={{ display: 'flex', justifyContent: 'space-around', flexWrap: 'wrap', flexDirection: 'row' }}>
         {filteringResult.map((data, index) => (
-          <div key={index} style={{ border: '1px solid black', flexBasis: '300px', margin: '10px', padding: '30px 50px', textAlign: 'center', fontWeight: 'bold' }}>
+          <div
+            key={index}
+            style={{
+              border: '0.1px solid white',
+              borderRadius: '8px',
+              flexBasis: '300px',
+              margin: '10px',
+              padding: '30px 50px',
+              textAlign: 'center',
+              fontWeight: 'bold',
+              backgroundColor: 'white',
+              boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.07)',
+            }}
+          >
             {data.klub}
           </div>
         ))}
