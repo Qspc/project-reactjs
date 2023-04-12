12342eb3e34d3b146277a5a68f056dd853b8263a
shibgotalloh <shibgotalloh_qqq@apps.ipb.ac.id>
2023-04-12 10:48:47 +0700

    update: finishing drawer and input number function

--- src/pages/Drawer.js
+++ src/pages/Drawer.js
@@ -1,3 +1,74 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Drawer from '@mui/material/Drawer';
+import List from '@mui/material/List';
+import Divider from '@mui/material/Divider';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import InboxIcon from '@mui/icons-material/MoveToInbox';
+import MailIcon from '@mui/icons-material/Mail';
+
 export default function DrawerFunc() {
-  return <></>;
+  const direction = ['atas', 'kanan', 'bawah', 'kiri'];
+  const dir = ['top', ' left', 'bottom', 'right'];
+
+  const [state, setState] = React.useState({
+    atas: false,
+    kanan: false,
+    bawah: false,
+    kiri: false,
+  });
+
+  const toggleDrawer = (anchor, open) => (event) => {
+    if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+      return;
+    }
+
+    setState({ ...state, [anchor]: open });
+  };
+
+  const list = (anchor) => (
+    <Box sx={{ width: anchor === 'atas' || anchor === 'bawah' ? 'auto' : 250 }} role="presentation" onClick={toggleDrawer(anchor, false)} onKeyDown={toggleDrawer(anchor, false)}>
+      <List>
+        {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+          <ListItem key={text} disablePadding>
+            <ListItemButton>
+              <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+      <Divider />
+      <List>
+        {['All mail', 'Trash', 'Spam'].map((text, index) => (
+          <ListItem key={text} disablePadding>
+            <ListItemButton>
+              <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
+
+  return (
+    <>
+      <div className="flex" style={{ alignItems: 'stretch', alignContent: 'center', gap: '20px', height: '100vh' }}>
+        {dir.map((data) => (
+          <>
+            <div style={{ padding: '0.75rem 4rem', border: '1px solid black', borderRadius: '10px', backgroundColor: '#F6F7C1' }} onClick={toggleDrawer(data, true)}>
+              {data}
+            </div>
+            <Drawer anchor={data} open={state[data]} onClose={toggleDrawer(data, false)}>
+              {list(data)}
+            </Drawer>
+          </>
+        ))}
+      </div>
+    </>
+  );
 }
--- src/pages/InputNumber.js
+++ src/pages/InputNumber.js
@@ -1,8 +1,45 @@
-export default function InputNumber() {
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
+export default function SearchInput() {
+  const [data, setData] = useState([]); //inisiasi pertama
+  const [searchResult, setSearchResult] = useState(''); //input yang dicari
+  const [result, setResult] = useState([]); //yg dipakai dalam looping
+
+  useEffect(() => {
+    if (searchResult) {
+      setResult(data.slice(0, searchResult));
+    } else {
+      setResult(data);
+    }
+  }, [searchResult, data]);
+
+  useEffect(() => {
+    axios
+      .get('https://restcountries.com/v3.1/all?fields=name')
+      .then((res) => {
+        setData(res.data);
+      })
+      .catch((error) => console.log(error));
+  }, []);
+
   return (
     <>
+      <h1>List of animal:</h1>
+
       <div>
-        <input type="number" />
+        <div style={{ margin: '20px 40px 40px 40px' }}>
+          <input style={{ padding: '0.2em 0.5em', font: 'inherit' }} onChange={(e) => setSearchResult(e.target.value)} type="number" placeholder="Input Number. . ." />
+        </div>
+
+        <div style={{ display: 'flex', flexDirection: 'row', flexWrap: 'wrap', justifyContent: 'flex-start', marginLeft: '40px' }}>
+          {result.map((data, index) => (
+            <div key={index} style={{ width: '250px', height: '3rem', marginLeft: '5px' }}>
+              {data.name.common}
+            </div>
+          ))}
+        </div>
+        <ul></ul>
       </div>
     </>
   );
