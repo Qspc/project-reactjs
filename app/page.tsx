278746101608b18ad41b4f1df8a95efea7a48c77
shibgotalloh <shibgotalloh_qqq@apps.ipb.ac.id>
2024-06-20 11:21:35 +0700

    update: home page

--- app/page.tsx
+++ app/page.tsx
@@ -5,47 +5,60 @@ import { useRouter } from 'next/navigation';
 
 export default function Home() {
   const router = useRouter();
+  const path = 'https://picsum.photos/300/200';
+
   const data = [
     {
-      image: 'https://picsum.photos/300/200',
+      image: path,
       title: 'Folder system',
-      description: 'make folder system like google drive',
+      description: 'Creating a folder system similar to Google Drive or the file storage system on a PC',
       link: '/folder-page',
     },
     {
-      image: 'https://picsum.photos/300/200',
-      title: 'Home Page Company',
-      description: 'make folder system like google drive',
+      image: path,
+      title: 'Homepage Company',
+      description: 'Homepage a business website for a company',
       link: '/homepage-website',
     },
   ];
-  const path = 'https://picsum.photos/300/200';
   return (
     <div>
-      <div className="px-20 py-10 ">
-        <div>Project React X Next Section</div>
-        <div>konten</div>
+      <div className="px-20 py-10 flex flex-col gap-10 ">
+        <div className="h-20">
+          <h1 className="font-bold text-[20px] uppercase ">Web Developing journal</h1>
+        </div>
+        <div className="w-full border-shadow-custom rounded-full">
+          <div className="flex h-[2rem] py-2 px-4 justify-between ">
+            <div></div>
+            <div></div>
+          </div>
+        </div>
         {/* card */}
         <div>
           <div className="flex w-full gap-10 flex-wrap">
             {data.map((value, index) => (
-              <div className=" w-[20%] h-80 border border-gray-100 shadow-lg flex flex-col" key={index}>
-                <div className=" border h-[140px] relative ">
-                  <Image fill={true} loader={() => value.image} src={value.image} alt="image" className="" />
+              <div className="w-[20%] h-80 flex flex-col group rounded-md border-shadow-custom gradient-background-custom " key={index}>
+                <div className=" h-[140px] m-[5px] rounded-md relative overflow-hidden">
+                  <Image className="rounded-md transform transition-transform duration-300 ease-in-out group-hover:scale-110" fill={true} loader={() => value.image} src={value.image} alt="image" />
                 </div>
-                <div className="py-2 px-2 flex flex-col justify-between h-[55%] ">
-                  <div>
-                    <div className="text-[22px] font-light capitalize ">{value.title}</div>
-                    <div className="text-[14px] font-semibold ">{value.description}</div>
+                <div className="py-2 px-2 flex flex-col gap-1 h-[55%] ">
+                  <div className="text-[20px] font-semibold capitalize ">{value.title}</div>
+                  <div className="text-[14px] font-light h-[45%] overflow-y-hidden ">
+                    {value.description.substring(0, 70)}
+                    {value.description.length > 70 ? '...' : ''}
                   </div>
-                  <div>
-                    <button className="text-gray-500 hover:text-black " onClick={() => router.push(`${value.link}`)}>
-                      more
-                    </button>
-                  </div>
+                  <button className="button-primer py-[6px] rounded-md font-semibold capitalize hover:bg-blue-400 " onClick={() => router.push(`${value.link}`)}>
+                    view
+                  </button>
                 </div>
               </div>
             ))}
+            <div className="w-[20%] h-80 flex flex-col group rounded-md border-shadow-custom gradient-background-custom ">
+              <div className=" h-[140px] m-[5px] rounded-md relative overflow-hidden">
+                <Image className="rounded-md transform transition-transform duration-300 ease-in-out group-hover:scale-110" fill={true} loader={() => path} src={path} alt="image" />
+              </div>
+              <div className="py-2 px-2 flex h-[55%] justify-center items-center text-gray-600 font-bold ">SOON</div>
+            </div>
           </div>
         </div>
       </div>
